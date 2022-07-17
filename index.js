847da1bc2fe86b100f7ca93b4021ff39cb7f6752
jimender2 <35303639+jimender2@users.noreply.github.com>
2022-07-17 12:12:24 -0400

    Fix missing semicolons

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const simpleGit = require('simple-git');
 const mkdirp = require('mkdirp');
 const fs = require('fs');
 const faker = require("faker");
-const path = require("path")
+const path = require("path");
 const cliProgress = require('cli-progress');
 
 
@@ -20,15 +20,15 @@ function generateRandomPost() {
 
 async function asyncCall(argv) {
 
-    let repoPath = ("path" in argv === true) ? argv["path"] : "path"
+    let repoPath = ("path" in argv === true) ? argv["path"] : "path";
 
-    let numberOfCommits = ("commits" in argv === true) ? Number(argv["commits"]) : 100
+    let numberOfCommits = ("commits" in argv === true) ? Number(argv["commits"]) : 100;
 
-    let initRepo = ("init" in argv === true) ? argv["init"] : false
+    let initRepo = ("init" in argv === true) ? argv["init"] : false;
 
-    let maxFilesPerCommit = ("maxFilesPerCommit" in argv === true) ? Number(argv["maxFilesPerCommit"]) : 100
+    let maxFilesPerCommit = ("maxFilesPerCommit" in argv === true) ? Number(argv["maxFilesPerCommit"]) : 100;
 
-    const postsDir = path.join(__dirname, repoPath)
+    const postsDir = path.join(__dirname, repoPath);
 
     mkdirp.sync(repoPath);
 
@@ -47,13 +47,13 @@ async function asyncCall(argv) {
 
     for (let i = 0; i < numberOfCommits; i++) {
 
-        let postTitles = ""
+        let postTitles = "";
         // Generate multiple posts
         for (let j = 0; j < Math.floor(Math.random() * maxFilesPerCommit); j++) {
             const post = generateRandomPost()
             const filePath = path.join(postsDir, `${post.title}.md`);
 
-            postTitles += `* ${post.title}\n`
+            postTitles += `* ${post.title}\n`;
 
             fs.writeFileSync(filePath, `---\ntitle: ${post.title}\nauthor: ${post.author}\ndate: ${post.date}\n---\n${post.body}`);
 
